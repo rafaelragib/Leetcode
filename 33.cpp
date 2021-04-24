d4e898aa2dfd4ce13bf154944e1d3651fd4436b9
Ragib <ragib.un.nur@gmail.com>
2021-04-24 14:32:18 +0600

    rotate array problem solved

---
+++ 33.cpp
@@ -0,0 +1,20 @@
+ int search(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]>nums[l]&& nums[l]<=target && target< nums[mid])
+                r=mid-1;
+            else if(nums[mid]<nums[r] && nums[r]>=target && target> nums[mid])
+                l=mid+1;
+            
+            else if(nums[mid]<nums[l])
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return -1;
+    }
