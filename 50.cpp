839a14c0e984281bb2a1023d9521839053d50c70
Ragib <ragib.un.nur@gmail.com>
2021-04-30 17:18:14 +0600

    pow(x,n) solved

---
+++ 50.cpp
@@ -0,0 +1,20 @@
+ double helper(double x,int n)
+    {
+        if(n==0)
+            return 1;
+       
+       double temp= helper(x,n2);
+    
+        if(n%2==0)
+        {
+            return temptemp;
+        }
+        else
+        {
+            return n0  1xtemptemp  temptempx;
+        }
+    }
+    double myPow(double x, int n) {
+      
+        return helper(x,n);
+    }
